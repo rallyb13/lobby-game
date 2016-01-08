f400fc6806a9c564cdc05982068cfb03b3bd05fb
Jeena <rakugama@users.noreply.github.com>
2016-01-08 13:52:22 -0800

    add symbol props

--- components/Token.js
+++ components/Token.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 var Token = React.createClass({
   render: function(){
-    var symbol = ['a'];
+    var symbol = this.props.symbol || ['a'];
 
     return (
       <div style={this.styles.token}>{symbol}</div>
