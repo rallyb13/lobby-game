70aaa0b7214ba379a85a914aad1a772dfccbf270
Benjamin Herson <benjaminherson@gmail.com>
2016-01-07 23:47:53 -0800

    fix grid square formatting (leaves border and has text for now to help next steps) with manually entered squares

--- components/Grid.js
+++ components/Grid.js
@@ -5,15 +5,21 @@ class Grid extends React.Component{
   render(){
     var styles = {
         mainGrid: {
-            backgroundColor: "green",
-            height: "300px",
-            width: "300px"
+            backgroundColor: "limegreen",
+            height: "900px",
+            width: "900px"
         }
-    }
+    };
 
     return (
       <div style={styles.mainGrid} >
-        <GridSquare />
+          <GridSquare />
+          <GridSquare />
+          <GridSquare />
+          <GridSquare />
+          <GridSquare />
+          <GridSquare />
+          <GridSquare />
       </div>
     );
   }
--- components/GridSquare.js
+++ components/GridSquare.js
@@ -5,13 +5,16 @@ class GridSquare extends React.Component{
     var styles = {
        gridSquare: {
         border: '1px solid #000',
-        height: '16.6%',
-        width: '16.6%'
+        height: '16.4%',
+        width: '16.4%',
+        color: 'white',
+        margin: 0,
+        display: 'inline-block'
        }
     }
 
     return (
-      <div style={styles.gridSquare} ></div>
+      <div style={styles.gridSquare} >Look at me! Eep Eep Eeep Eeeeep</div>
     );
   }
 
