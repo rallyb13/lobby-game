cd76e3e8ae8dbf864985bc9a631d817fd873f058
Jenny Olsen <jenny.a.olsen@gmail.com>
2016-07-31 15:31:33 -0700

    final oil lobby help image in place

--- components/Overlay/OilLobbyHelp.js
+++ components/Overlay/OilLobbyHelp.js
@@ -5,15 +5,16 @@ var OilLobbyHelp = React.createClass({
     return (
       <div>
         <p>Match 3 oil drops together (non-diagonally) to create an oil can.</p>
-        <img src="assets/help-images/match3oil.gif" />
+        <img className="short-help-image" src="assets/help-images/match3oil.gif" />
         <p>Oil cans combine to make <b>derricks</b>, which make <b>pipelines</b>, which make <b>refineries</b>.</p>
         <div className="inline-help-images">
             <img src="assets/icons/icon-derrick.png" />
             <img src="assets/icons/icon-pipeline.png" />
             <img src="assets/icons/icon-refinery.png" />
         </div>
-        <p>Refineries don&#39;t have to be placed together. Once you get 5 refineries onto the board, the oil lobby awards you a Favor, which appears on the helper bench above the game board.</p>
+        <p>Refineries don&#39;t have to be placed together. Once you get 5 refineries onto the board, the oil lobby awards you a <b>Favor</b>, which appears on the helper bench above the game board.</p>
         <p>Click on the oil slick favor, and it becomes your next token. Place it anywhere on the board, and that row will get cleared out.</p>
+        <img src="assets/help-images/oilslick.gif" />
       </div>
     )
   }
