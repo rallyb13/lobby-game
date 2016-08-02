dd4db1790b641d26c3232cc4744d2cb704f384ef
Benjamin Herson <benjaminherson@gmail.com>
2016-08-01 21:20:35 -0700

    add images/styling to agribiz help in overlay

--- components/Overlay/Helps/AgribusinessLobbyHelp.js
+++ components/Overlay/Helps/AgribusinessLobbyHelp.js
@@ -4,10 +4,20 @@ var AgribusinessLobbyHelp = React.createClass({
   render(){
     return(
       <div>
-        <p>The Agribusiness Lobby works much like the Oil Lobby. Match three patented (and non-reproducing) seeds together and you get a corn token.</p>
-        <p>The corns combine to make some fat cattle, which combine to make slaughter houses, which combine to make Fat Food restaurants.</p>
-        <p>The Fat Food joints don&#39;t have to be placed together. Once you have enough, you&#39;ll earn a Favor from the Agribusiness lobby.</p>
-        <p>Use the Corn Syrup/Itis to hold those pesky constituents in place for ten moves when you really need them to stay out of your way. Over-eating can make them lethargic.</p>
+        <p>The Agribusiness Lobby works much like the Oil Lobby. Match three patented (and non-reproducing) <b>seeds</b> together and you get a <b>corn</b> token.</p>
+        <div className="inline-help-images">
+          <img src="assets/icons/icon-seeds.png" />
+          <img src="assets/icons/icon-corn.png" />
+        </div>
+        <p>The corns combine to make some fat <b>cattle</b>, which combine to make <b>slaughter houses</b>, which combine to make <b>Fat Food restaurants</b>.</p>
+        <div className="inline-help-images">
+          <img src="assets/icons/icon-cattle.png" />
+          <img src="assets/icons/icon-slaughter.png" />
+          <img src="assets/icons/icon-restaurant.png" />
+        </div>
+        <p>The Fat Food joints don&#39;t have to be placed together. Once you have enough, you&#39;ll earn a <b>Favor</b> from the Agribusiness lobby.</p>
+        <img src="assets/icons/icon-greasefire.png" /> 
+        <p>Use the <b>Corn Syrup/Itis favor</b> to hold those pesky constituents in place for ten moves when you really need them to stay out of your way. Over-eating can make them lethargic.</p>
       </div>
     )
   }
--- components/Overlay/Helps/OilLobbyHelp.js
+++ components/Overlay/Helps/OilLobbyHelp.js
@@ -4,7 +4,7 @@ var OilLobbyHelp = React.createClass({
   render(){
     return (
       <div>
-        <p>Match 3 oil drops together (non-diagonally) to create an oil can.</p>
+        <p>Match 3 <b>oil drops</b> together (non-diagonally) to create an <b>oil can</b>.</p>
         <img className="short-help-image" src="assets/help-images/match3oil.gif" />
         <p>Oil cans combine to make <b>derricks</b>, which make <b>pipelines</b>, which make <b>refineries</b>.</p>
         <div className="inline-help-images">
@@ -13,7 +13,7 @@ var OilLobbyHelp = React.createClass({
             <img src="assets/icons/icon-refinery.png" />
         </div>
         <p>Refineries don&#39;t have to be placed together. Once you get 5 refineries onto the board, the oil lobby awards you a <b>Favor</b>, which appears on the helper bench above the game board.</p>
-        <p>Click on the oil slick favor, and it becomes your next token. Place it anywhere on the board, and that row will get cleared out.</p>
+        <p>Click on the <b>oil slick favor</b>, and it becomes your next token. Place it anywhere on the board, and that row will get cleared out.</p>
         <img src="assets/help-images/oilslick.gif" />
       </div>
     )
