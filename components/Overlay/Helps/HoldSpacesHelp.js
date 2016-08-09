a6db0b77f6101810754a7938e6fda7bb8dc67df1
Benjamin Herson <benjaminherson@gmail.com>
2016-08-08 21:43:41 -0700

    add gif help images for hold spaces helper

--- components/Overlay/Helps/HoldSpacesHelp.js
+++ components/Overlay/Helps/HoldSpacesHelp.js
@@ -6,7 +6,9 @@ var HoldSpacesHelp = React.createClass({
       <div>
         <p>As you work to satisfy multiple lobbies, it becomes increasingly difficult to rely on getting the piece you want when you want it.</p>
         <p>The Hold spaces make this work easier. Click on an empty hold space and the piece that is next to be played will be held there.</p>
+        <img src="assets/help-images/hold1.gif" />
         <p>When you are ready to play it, simply click the piece in the hold space and it will become the next piece to be placed.</p>
+        <img src="assets/help-images/hold2.gif" />
         <p>Keep in mind, though, that these spaces are costly. The first space to appear costs you $10 per move to hold it there (everything in Washington has a price, after all).</p>
         <p>The second hold space that appears will cost you $100 per move and the third will cost you $1000, so you had better really need to keep it there, and you&#39;ll want to use it quick!</p>
       </div>
